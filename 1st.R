0c146c3ee2c736fe0521f01344bde6b0ef31b7f7
Shimi Zhou <33765888+ShimiZhou@users.noreply.github.com>
2022-09-17 14:42:11 -0400

    Add files via upload

---
+++ 1st.R
@@ -0,0 +1,19 @@
+A <- 3
+b <- 4
+E = 8
+C=A +b +E
+C
+var1 <- 2.5
+var2 <- 2
+typeof(var2)
+RESULT <- var1 * var2
+RESULT 
+
+answer <- sqrt(var2)
+answer
+
+greeting <- "good afternoon"
+name <-"shimi"
+message <- paste(greeting,name)
+message
+
---
+++ section2_logical variables.R
@@ -0,0 +1,14 @@
+x <- rnorm(5)
+x
+for(i in x){
+  print(i)
+}
+
+print(x[1])
+print(x[2])
+x[3]
+x[4]
+x[5]
+?rnorm
+?c()
+
